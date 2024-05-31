53b8c7023134c652eea8972d9818f61b8f64f094
Ghoratt <maylleluis735@gmail.com>
2024-05-30 22:02:54 -0500

    añadir suma en c++

---
+++ suma.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n1=0;
+    int n2=0;
+
+    cout<<"hola mundo, ingrese dos numeros\n";
+    cout<<"numero 1: "; cin>>n1;
+    cout<<"\nnumero 2: "; cin>>n2;
+    cout<<"\nla suma es: "<<n1+n2;
+
+    return 0;
+}
